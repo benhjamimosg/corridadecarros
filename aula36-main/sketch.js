a927ae4fcede95d09aaaa2756ba3180aa8e71c62
benhjamimosg <92053962+benhjamimosg@users.noreply.github.com>
2022-07-08 10:04:01 -0300

    Add files via upload

---
+++ aula36-main/sketch.js
@@ -0,0 +1,39 @@
+var canvas, backgroundImage;
+var gameState=0
+var playerCount;
+var distance=0
+var database;
+
+var form, player, game;
+
+var allPlayers;
+var cars, car1, car2, car3, car4;
+
+function preload(){
+  car1_img = loadImage("car1.png");
+  car2_img = loadImage("car2.png");
+  car3_img = loadImage("car3.png");
+  car4_img = loadImage("car4.png");
+
+  track = loadImage("track.jpg");
+  ground = loadImage("ground.png");
+}
+function setup(){
+  canvas = createCanvas(displayWidth -20, displayHeight -30)
+  database=firebase.database();
+
+  game=new Game();
+  game.getState();
+  game.start();
+}
+
+function draw(){
+  if (playerCount=== 4) {
+    game.update(1)
+  }
+
+  if (gameState=== 1) {
+    clear();
+    game.play()
+  }
+}
